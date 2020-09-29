964d0e5b90bf3fa54a24c47635c25dfa6ee1b543
Luiz Castilho <luizucastilho@gmail.com>
2020-09-28 22:22:27 -0300

    Create User Service test created

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,47 @@
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import { uuid } from 'uuidv4';
+import User from '../../infra/typeorm/entities/user';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findByID(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.id === id);
+
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => user.email === email);
+
+    return findUser;
+  }
+
+  public async create({
+    name,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid(), name, email, password });
+
+    this.users.push(user);
+
+    this.save(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;
