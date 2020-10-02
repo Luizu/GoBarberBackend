d3aa3039691b81273e79d8c565a146da2194b27d
Luiz Castilho <luizucastilho@gmail.com>
2020-10-02 07:06:00 -0300

    Providers list feature created

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -23,7 +23,7 @@ class AuthenticateUserService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('hashProvider')
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
   ) {}
 
